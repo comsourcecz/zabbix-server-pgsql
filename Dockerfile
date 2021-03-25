a703732e42666cd6a528b9e612542fcb62c63c92
Jaroslav Barton <jaroslav.barton@comsource.cz>
2021-03-25 11:38:36 +0100

    Added call script for twilio, updated Dockerfile for two stage build

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
-FROM zabbix/zabbix-server-pgsql:ubuntu-5.2-latest
+FROM zabbix/zabbix-server-pgsql:ubuntu-5.2-latest AS builder
 
 USER root
 
 RUN apt update \
-    && apt install --no-install-recommends -y python3 python3-pysnmp4 \
+    && apt install --no-install-recommends -y python3 python3-pip \
+    && pip3 install twilio pysnmp
+
+
+FROM zabbix/zabbix-server-pgsql:ubuntu-5.2-latest
+USER root
+RUN apt update \
+    && apt install -y --no-install-recommends python3 \
     && rm -rf /var/lib/apt/lists/*
-
+COPY --from=builder /usr/local/lib/python3.8 /usr/local/lib/python3.8
 ADD discovery_juniper_rpm.py /usr/lib/zabbix/externalscripts/discovery_juniper_rpm.py
-
+ADD call.py /usr/lib/zabbix/alertscripts/call.py
 USER zabbix
---
+++ call.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import sys
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+PHONE_NO = os.environ['TWILIO_PHONE_NO']
+
+if len(sys.argv) < 3:
+    print("error")
+    sys.exit(1)
+
+TO=sys.argv[1]
+SUBJECT=sys.argv[2]
+BODY=sys.argv[3]
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+call = client.calls.create(
+                        twiml=f'<Response><Pause length="2"/><Say voice="woman" loop="3">{SUBJECT}: {BODY}</Say></Response>',
+                        to=TO,
+                        from_=PHONE_NO
+                    )
